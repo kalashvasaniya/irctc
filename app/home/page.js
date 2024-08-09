11a570d746c0e7ded7b23ba3583f3ee9f56865ac
kalash <kalashvasaniya@gmail.com>
2024-08-09 20:26:48 +0530

    User panel & Admin Panel structure

--- app/home/page.js
+++ app/home/page.js
@@ -31,11 +31,41 @@ export default function Home() {
     fetchUser();
   }, []);
 
+  const logout = () => {
+    localStorage.removeItem('token');
+    window.location.href = '/';
+  }
+
   return (
-    <div className="text-pink-500">
-      IRCTC done
-      <p>Email: {email}</p>
-      <p>Name: {name}</p>
+    <div className="text-sky-400">
+      <nav class="fixed py-6 w-full bg-black z-50">
+        <div class="container flex items-center justify-between mx-auto px-8 md:px-14 lg:px-20 w-full">
+
+          <div className='flex flex-row space-x-1'>
+            <div className="text-lg font-semibold">Welcome to IRCTC,</div>
+            <div class="text-lg font-semibold text-gray-200 hover:text-white underline-offset-2 underline decoration-sky-400">
+              {name}</div>
+          </div>
+
+          <div class="hidden md:flex space-x-12 items-center text-gray-400">
+            <button onClick={logout}
+              class="p-2 px-4 text-lg rounded-sm text-white font-bold transition delay-150 bg-red-500  hover:bg-red-600 duration-300">Logout</button>
+          </div>
+        </div>
+      </nav>
+
+      {email === "kalashvasaniya@gmail.com" ? (
+        <div className="pt-24">
+          <div className="">
+            <div className="text-xl font-semibold text-center font-mono">Admin Panel</div>
+            <div className="text-lg font-semibold text-center font-mono">You are an admin</div>
+          </div>
+        </div>
+      ) : (
+        <div className="pt-24">
+          <div className="text-xl font-semibold text-center font-mono">User Panel</div>
+        </div>
+      )}
     </div>
   );
 }
